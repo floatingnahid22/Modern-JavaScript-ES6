637a3d4126978933e52b897f129ad1d7f409c396
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-14 16:58:11 +0600

    array-cpmr-method pushed

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-var numbers = [1, 2, 3, 4, 12,14];
+//array concat()
+var numbers1 = [1, 2, 3, 4, 5];
+var numbers2 = [6, 7, 8, 9, 10];
+var numbers3 = [11, 12, 13, 14, 15];
 
-var result = numbers.splice(1, 2, 10,19,15);//(divider position, elements to be deleted, elements will be added)
+var result = numbers2.concat(numbers3)
 
-console.log(result);
-console.log(numbers);
+//array push()-push as a element of a array, you will see in result
+numbers1.push([6,7]) 
+
+//array map()-work like for loop
+var result2 = numbers2.map((num) => {
+    return 2* num;
+});
+
+
+//array resuce()- reduced a array into a single value
+var sum = numbers3.reduce((prevValue, currentValue, currentIndex, arr) => {
+    return prevValue+currentValue;
+});
+
+
+console.log("concat:" + result)
+console.log("push:" + numbers1)
+console.log("map:" + result2)
+console.log("reduce:" + sum)
 
