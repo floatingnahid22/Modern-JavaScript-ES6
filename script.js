6bc0af303758f444ea4122f1c9060356c2ff2669
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-14 15:17:17 +0600

    ternary-operator pushed

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-var myVar = "";
+// var age = 18;
 
-//False, 0, "", null, undefined, Nan- Falsy
+// //it's complex, so used for 1 or 2level if-else
+// var type = age >= 18 ? "adult" : age<10 ? "Child" : "Young";
+// console.log(type);
 
+// var a = 5;
+// var access = a>5 ? true : false;
+// console.log(access); 
+//same as below code
 
-if(myVar) {
-    console.log("I am truthy");
-} else {
-    console.log("I am falsy");
-}
+var a = 5;
+var access = a>5 ;
+console.log(access);
