7504efb4755663fd7fd6b60551b07a7a282e23d6
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-14 15:44:06 +0600

    array-filter-method

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var numbers = [1, 2, 3, 4, 12,14];
 
-var result = numbers.findIndex((currentValue, index, arr) => {
-    return currentValue > 20;
+var result = numbers.filter((currentValue, index, arr) => {
+    return currentValue > 4;
 });
 
 console.log(result);
