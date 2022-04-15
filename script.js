65545230c3ee31681547d811bdf8391664fd2842
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-15 14:09:55 +0600

    rest-operator pushed

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
- //array spreading
- 
- var numbers = [1,2,3,4,5];
+function myFunc(a,...params) { //params should be in last as a parameter
+    console.log(a, params); 
+}
 
- var a = [...numbers];//exact copy of numbers
-
- var newNumbers = [...numbers,6];
- numbers.push(7)
-
- console.log(newNumbers);
- console.log(numbers);
-
- //object spreading 
-
- var myObj1 = {
-     x:1,
-     y:2
- }
- var myObj2 = {
-     a:1,
-     b:2
- }
-
- console.log({
-    ...myObj1,
-    ...myObj2
- });
+myFunc(4, 5, 6, 7, 8, 9);
