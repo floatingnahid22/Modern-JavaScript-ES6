5ef39ae7193b2594e54ca34c277b544b63d15b4f
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-15 16:07:13 +0600

    copywithin pushed

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-//template literals
-var a = 5;
-var b = 6;
+// start and end er majhkhaner element ta ke target er jaygay bosay dey. copyWithin(target, start, end)
 
-console.log(`I am ${a+b} and I am ${a-b}`);
+let a = ['JavaScript','PHP','Python','Ruby','C++','RUST']
 
-//tagged template literals
-function modifier(strings, ...values) {
-    // console.log(strings);
-    // console.log(values);
-    const m = strings.reduce((prev, current) => {
-        return prev + current +(values.length ? "Mr. " + values.shift(): "")
-    }, "");
-    return m;
-}
-
-var player1 = 'sakib';
-var player2 = 'tamim';
-
-console.log(modifier`We have ${player1} and ${player2} in Bangladesh Cricket Team.`);
+a.copyWithin(-3, -2, -1);
